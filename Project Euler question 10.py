3a276443b9732500bc6b99e2589b9604df8f5119
StealthyDrake12 <98287300+StealthyDrake12@users.noreply.github.com>
2025-08-16 12:58:08 +0100

    done

--- Project Euler question 10.py
+++ Project Euler question 10.py
@@ -4,8 +4,7 @@ import math
 
 total = 0
 
-#function
-
+#function - ##Taken from internet to check primes, as my previous solution was slow and inefficient###
 def isPrime(num):
     if num < 2:
         return False
@@ -19,6 +18,7 @@ def isPrime(num):
 for i in range (2, 2000000):
     if isPrime(i):
         total += i
+
 
 
 print (total)
